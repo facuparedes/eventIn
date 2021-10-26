45362524e2a879eaca5bf999d8d0ec59b725d463
Facundo Paredes <fparedes45@gmail.com>
2021-10-26 16:08:39 -0300

    Fix conflict

--- App.js
+++ App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import Login from './screens/Login';
-import Register from './screens/Register';
-import { Provider } from 'react-redux';
-import store from './store/store.js';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
-
+import React from "react";
+import { Provider } from "react-redux";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import Login from "./src/screens/Login";
+import Register from "./src/screens/Register";
+import store from "./src/common/redux/store.js";
 
 export default function App() {
-
   const Stack = createStackNavigator();
-  
+
   return (
     <Provider store={store}>
-        <NavigationContainer>
+      <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="Login" component={Login}/>
-          <Stack.Screen name="Register" component={Register}/>
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="Register" component={Register} />
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
-
   );
 }
-
