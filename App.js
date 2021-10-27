9474b0c9398185fde34dcc711fd2b2ab97578b7d
cristian barriento <80364197+cristiangaby112@users.noreply.github.com>
2021-10-27 14:18:17 -0300

    initializate TabBar

--- App.js
+++ App.js
@@ -1,21 +1,14 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
-import Login from "./src/screens/Login.jsx";
-import Register from "./src/screens/Register.jsx";
 import store from "./src/common/redux/store.js";
+import TabBar from "./src/routes/TabBar";
 
 export default function App() {
-  const Stack = createStackNavigator();
-
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Login" component={Login} />
-          <Stack.Screen name="Register" component={Register} />
-        </Stack.Navigator>
+        <TabBar />
       </NavigationContainer>
     </Provider>
   );
