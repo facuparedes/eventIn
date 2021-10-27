10c1ee57e7a2d7dee76d3ffe64a1bf65bf447460
Christian-000 <christianpiccinini4@gmail.com>
2021-10-27 19:25:02 -0300

    Form fields in progress

--- App.js
+++ App.js
@@ -6,6 +6,8 @@ import Login from "./src/screens/Login.jsx";
 import Register from "./src/screens/Register.jsx";
 import store from "./src/common/redux/store.js";
 
+//BORRAR LUEGO
+import FormEvent from "./src/screens/FormEvent/FormEvent.jsx";
 export default function App() {
   const Stack = createStackNavigator();
 
@@ -13,6 +15,8 @@ export default function App() {
     <Provider store={store}>
       <NavigationContainer>
         <Stack.Navigator>
+          <Stack.Screen name="form" component={FormEvent} />
+
           <Stack.Screen name="Login" component={Login} />
           <Stack.Screen name="Register" component={Register} />
         </Stack.Navigator>
--- src/screens/FormEvent/FormEvent.jsx
+++ src/screens/FormEvent/FormEvent.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-import { Input } from 'react-native-elements' 
+import React, {useState} from 'react'
+import { View } from 'react-native'
+import { Input, CheckBox, Text, Button } from 'react-native-elements' 
+import { ScrollView } from 'react-native-gesture-handler'
 
 const FormEvent = () => {
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+    const [fee, setFee] = useState('')
+    //date
+    const [isPublic, setIsPublic] = useState('')
+    const [isPrivate, setIsPrivate] = useState('')
+
+    
     return (
+//titulo -description - $fee - date - isPublic - location = {lat, long} - attachments - createdAt
+        
         <View>
-            <Input placeholder="Event title..."/>
+            <ScrollView>
+            <Input label="Titulo" placeholder="Titulo del evento..."/>
+            <Input label="Descripción" placeholder="Descripcion ..."/>
+            <Input label="Tarifa" placeholder="Evento fee..."/>
+            <Input label="Fecha" placeholder="Evento date..."/>
+            <Text h4>Tipo de evento</Text>
+            <CheckBox title="Publico"/>
+            <CheckBox title="Privado"/>
+
+            {/* <Input label="Ubicacion" placeholder="Evento location..."/> */}
+            <Input label="Fotos" placeholder="Añadir link de la foto..."/>
+            {/*createdAt*/}
+            <Button title="Crear Evento" />
+            </ScrollView>
+
         </View>
     )
 }
