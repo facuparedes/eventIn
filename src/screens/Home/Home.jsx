fd66bceb8eed074c815524ffd82a89e27f9aa26f
LucianaHer <lucianaher74@gmail.com>
2021-10-27 20:22:04 -0300

    screens folders created

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
-export default function Login () {
+export default function Home () {
     return(
         <View>
             <Text>Hola soy el home</Text>
