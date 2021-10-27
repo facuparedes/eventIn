80f97c770ab9802afe4c45f3f2b4ae60a2e418b1
Christian-000 <christianpiccinini4@gmail.com>
2021-10-27 20:00:01 -0300

    added local states and handles

--- src/screens/FormEvent/FormEvent.jsx
+++ src/screens/FormEvent/FormEvent.jsx
@@ -6,25 +6,50 @@ import { ScrollView } from 'react-native-gesture-handler'
 const FormEvent = () => {
     const [title, setTitle] = useState('')
     const [description, setDescription] = useState('')
-    const [fee, setFee] = useState('')
+    const [fee, setFee] = useState(0)
     //date
-    const [isPublic, setIsPublic] = useState('')
-    const [isPrivate, setIsPrivate] = useState('')
-
-    
+    const [isPublic, setIsPublic] = useState(false)
+    const [isPrivate, setIsPrivate] = useState(false)
+    // HANDELS FUNCTIONS
+    const handleTitle = (text) => {
+        setTitle(text)
+    }
+    const handleDescription = (text) => {
+        setDescription(text)
+    }
+    const handleFee = (value) => {
+        setFee(value)
+    }
+    const handleIsPublic = () => {
+        setIsPublic(!isPublic)
+        console.log(isPublic)
+    }
+    const handleIsPrivate = () => {
+        setIsPrivate(!isPrivate)
+    }
     return (
 //titulo -description - $fee - date - isPublic - location = {lat, long} - attachments - createdAt
         
         <View>
             <ScrollView>
-            <Input label="Titulo" placeholder="Titulo del evento..."/>
-            <Input label="Descripción" placeholder="Descripcion ..."/>
-            <Input label="Tarifa" placeholder="Evento fee..."/>
-            <Input label="Fecha" placeholder="Evento date..."/>
-            <Text h4>Tipo de evento</Text>
-            <CheckBox title="Publico"/>
-            <CheckBox title="Privado"/>
+            <Input 
+                label="Titulo" 
+                placeholder="Titulo del evento..." 
+                onChangeText={handleTitle}/>
+            <Input 
+                label="Descripción" 
+                placeholder="Descripcion ..."
+                onChange={handleDescription}/>
 
+            <Input label="Tarifa" placeholder="Tarifa..."/>
+
+
+            <Input label="Fecha del evento" placeholder="Evento date..."/>
+            <Text h4>Tipo de evento</Text>
+            <View>
+                <CheckBox title="Publico" onPress={handleIsPublic}/>
+                <CheckBox title="Privado" onPress={handleIsPrivate}/>
+            </View>
             {/* <Input label="Ubicacion" placeholder="Evento location..."/> */}
             <Input label="Fotos" placeholder="Añadir link de la foto..."/>
             {/*createdAt*/}
