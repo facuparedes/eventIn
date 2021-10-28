8c9890fcab7ab946c66cabc2118c4c58b2d5b2db
santicardona04 <santiago.cardona1@gmail.com>
2021-10-27 21:28:42 -0300

    Styles in progress

--- src/screens/FormEvent/FormEvent.jsx
+++ src/screens/FormEvent/FormEvent.jsx
@@ -1,70 +1,89 @@
-import React, {useState} from 'react'
-import { View } from 'react-native'
-import { Input, CheckBox, Text, Button } from 'react-native-elements' 
-import { ScrollView } from 'react-native-gesture-handler'
-
+import React, { useState } from "react";
+import { View } from "react-native";
+import { Input, CheckBox, Text, Button } from "react-native-elements";
+import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
+import styles from "./FormStyles";
 const FormEvent = () => {
-    const [title, setTitle] = useState('')
-    const [description, setDescription] = useState('')
-    const [fee, setFee] = useState(0)
-    //date
-    const [isPublic, setIsPublic] = useState(false)
-    const [isPrivate, setIsPrivate] = useState(false)
-    // HANDLES FUNCTIONS
-    const handleTitle = (text) => {
-        setTitle(text)
-    }
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [fee, setFee] = useState(0);
+  //date
+  const [isPublic, setIsPublic] = useState(false);
+  const [isPrivate, setIsPrivate] = useState(false);
+  // HANDLES FUNCTIONS
+  const handleTitle = (text) => {
+    setTitle(text);
+  };
 
-    const handleDescription = (text) => {
-        setDescription(text)
-    }
-    const handleFee = (value) => {
-        setFee(value)
-    }
+  const handleDescription = (text) => {
+    setDescription(text);
+  };
+  const handleFee = (value) => {
+    setFee(value);
+  };
 
-    const handleIsPublic = () => {
-        setIsPublic(!isPublic)
-    }
-    
-    const handleIsPrivate = () => {
-        setIsPrivate(!isPrivate)
-    }
+  const handleIsPublic = () => {
+    setIsPublic(!isPublic);
+  };
 
-    const handleSubmit = () => {
+  const handleIsPrivate = () => {
+    setIsPrivate(!isPrivate);
+  };
 
-    }
+  const handleSubmit = () => {};
 
-    return (
-//titulo -description - $fee - date - isPublic - location = {lat, long} - attachments - createdAt
-        
-        <View>
-            <ScrollView>
-            <Input 
-                label="Titulo" 
-                placeholder="Nombre del evento..." 
-                onChangeText={text => handleTitle(text)}/>
-            <Input 
-                label="Descripción" 
-                placeholder="Descripción..."
-                onChange={handleDescription}/>
+  return (
+    //titulo -description - $fee - date - isPublic - location = {lat, long} - attachments - createdAt
 
-            <Input label="Tarifa" placeholder="Tarifa..."/>
+    <View style={styles.container}>
+      <ScrollView>
+        <Input 
+        label="Titulo" 
+        placeholder="Nombre del evento..."
+        onChangeText={(text) => handleTitle(text)} 
+        inputStyle={styles.input}
+        labelStyle={styles.label}
+        inputContainerStyle={styles.inputcont}/>
 
+        <Input 
+        label="Descripción" 
+        placeholder="Descripción..." 
+        onChange={handleDescription} 
+        inputStyle={styles.input}
+        labelStyle={styles.label}
+        inputContainerStyle={styles.inputcont}/>
 
-            <Input label="Fecha del evento" placeholder="Fecha..."/> 
-            <Text h4>Tipo de evento</Text>
-            <View>
-                <CheckBox title="Publico" onPress={handleIsPublic}/>
-                <CheckBox title="Privado" onPress={handleIsPrivate}/>
-            </View>
-            {/* <Input label="Ubicacion" placeholder="Evento location..."/> */}
-            <Input label="Fotos" placeholder="Añadir link de la foto..."/>
-            {/*createdAt*/}
-            <Button title="Crear Evento" onPress={handleSubmit} />
-            </ScrollView>
+        <Input 
+        label="Tarifa" 
+        placeholder="Tarifa..."
+        inputStyle={styles.input}
+        labelStyle={styles.label}
+        inputContainerStyle={styles.inputcont}/>
 
+        <Input 
+        label="Fecha del evento" 
+        placeholder="Fecha..." 
+        inputStyle={styles.input}
+        labelStyle={styles.label}
+        inputContainerStyle={styles.inputcont}/>
+       
+        <Text h4>Tipo de evento</Text>
+        <View style={styles.checkBox}>
+          <CheckBox title="Publico" onPress={handleIsPublic} size={20}/>
+          <CheckBox title="Privado" onPress={handleIsPrivate} />
         </View>
-    )
-}
+        {/* <Input label="Ubicacion" placeholder="Evento location..."/> */}
+        <Input label="Fotos" placeholder="Añadir link de la foto..." 
+        inputStyle={styles.input}
+        labelStyle={styles.label}
+        inputContainerStyle={styles.inputcont}/>
+        {/*createdAt*/}
+        <TouchableOpacity title="Crear Evento" onPress={handleSubmit} style={styles.btn}>
+        <Text>Crear Evento</Text>
+        </TouchableOpacity> 
+      </ScrollView>
+    </View>
+  );
+};
 
-export default FormEvent
+export default FormEvent;
