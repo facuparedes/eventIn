074dbfc1b23a8cfc277f558b617bfba600cdd59a
CrisCorzo97 <CrCorzo04@hotmail.com>
2021-10-27 22:48:20 -0300

    created Onboarding Screen and modified App.js

--- src/routes/TabBar.jsx
+++ src/routes/TabBar.jsx
@@ -1,57 +1,50 @@
-import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Entypo, Feather, FontAwesome } from '@expo/vector-icons';
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Entypo, Feather, FontAwesome } from "@expo/vector-icons";
 
-import Home from '../screens/Home';
-import Login from '../screens/Login';
-import Register from '../screens/Register';
+import Home from "../screens/Home/Home.jsx";
+import Login from "../screens/Login/Login";
+import Register from "../screens/Register/Register";
 
 const Tab = createBottomTabNavigator();
 
 export default function TabBar() {
   return (
     <Tab.Navigator
-      
       initialRouteName="Home"
       screenOptions={{
         tabBarStyle: {
-          backgroundColor: '#121212',
-          borderTopColor: 'transparent',
+          backgroundColor: "#121212",
+          borderTopColor: "transparent",
           paddingBottom: 5,
           paddingTop: 5,
         },
-        tabBarActiveTintColor: '#91c7fb',
-        tabBarInactiveTintColor: '#fff',
+        tabBarActiveTintColor: "#91c7fb",
+        tabBarInactiveTintColor: "#fff",
       }}
     >
-      <Tab.Screen 
-      name="Home" 
-      component={Home} 
-      options={{
-        tabBarIcon: ({ color, size }) => (
-          <Entypo name="home" color={color} size={size} />
-        )
-      }}
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color, size }) => <Entypo name="home" color={color} size={size} />,
+        }}
       />
 
-      <Tab.Screen 
-      name="Login" 
-      component={Login} 
-      options={{
-        tabBarIcon: ({ color, size }) => (
-          <Entypo name="login" color={color} size={size} />
-        )
-      }}
+      <Tab.Screen
+        name="Login"
+        component={Login}
+        options={{
+          tabBarIcon: ({ color, size }) => <Entypo name="login" color={color} size={size} />,
+        }}
       />
 
-      <Tab.Screen 
-      name="Register" 
-      component={Register}
-      options={{
-        tabBarIcon: ({ color, size }) => (
-          <FontAwesome name="sign-in" color={color} size={size} />
-        )
-      }}
+      <Tab.Screen
+        name="Register"
+        component={Register}
+        options={{
+          tabBarIcon: ({ color, size }) => <FontAwesome name="sign-in" color={color} size={size} />,
+        }}
       />
     </Tab.Navigator>
   );
--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React from "react";
+import { View, Text } from "react-native";
 
-export default function Home () {
-    return(
-        <View>
-            <Text>Hola soy el home</Text>
-        </View>
-    )
+export default function Home() {
+  return (
+    <View>
+      <Text>Hola soy el home</Text>
+    </View>
+  );
 }
