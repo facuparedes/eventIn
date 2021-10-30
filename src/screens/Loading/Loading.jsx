332a343508160c3243386035533944d069febb4a
cristian barriento <80364197+cristiangaby112@users.noreply.github.com>
2021-10-30 15:36:03 -0300

    Loading

---
+++ src/screens/Loading/Loading.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { ActivityIndicator, StyleSheet, View, Image, Text } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  logo: {
+    marginBottom: 100,
+    width: 320,
+    height: 142,
+  },
+  text: {
+    fontSize: 20,
+    marginTop: 10,
+  }
+});
+
+function Loading(loading, children) {
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <Image style={styles.logo} source={require("../../assets/Logo.png")} />
+        <ActivityIndicator size="large" color="#00ff00" />
+        <Text style={styles.text}>Loading...</Text>
+      </View>
+    );
+  }
+    return children;
+}
+
+export default Loading;
