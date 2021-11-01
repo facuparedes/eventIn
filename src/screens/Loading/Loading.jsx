a754086a5888321116acaf5c361fe1d30aa87fcc
cristian barriento <80364197+cristiangaby112@users.noreply.github.com>
2021-11-01 20:02:49 -0300

    loading con event global

--- src/screens/Loading/Loading.jsx
+++ src/screens/Loading/Loading.jsx
@@ -1,29 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { ActivityIndicator, View, Image, Text } from "react-native";
 import { styles } from "./styles";
-import {useSelector} from "react-redux";
-
+import { useSelector, useDispatch } from "react-redux";
+import { getEvents } from "../../common/redux/actions";
 
 export default function Loading({ navigation }) {
-  const allEvents = useSelector((state) => state.events)
+  const allEvents = useSelector((state) => state.events);
+  const dispatch = useDispatch();
 
-  const timer = function () {
-    setTimeout(() => {
-      navigation.replace("TabBar");
-    }, 1000);
-  };
+  useEffect(() => {
+    dispatch(getEvents());
+  }, [dispatch]);
 
   return (
     <View style={styles.container}>
-      {allEvents.length > 0 ? timer() : null}
+      {allEvents.length === 0 ? null : navigation.replace("TabBar")}
       <Image style={styles.logo} source={require("../../assets/Logo.png")} />
       <ActivityIndicator size="large" color="#00ff00" />
       <Text style={styles.text}>Loading...</Text>
     </View>
   );
 }
-
-
-
-
-
