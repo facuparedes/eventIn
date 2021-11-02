8c7920b0889d5fa2921619256b1d3d57139561ca
santicardona04 <santiago.cardona1@gmail.com>
2021-11-02 01:15:06 -0300

    style details

--- src/screens/Card Detail/CardDetail.jsx
+++ src/screens/Card Detail/CardDetail.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import { Text, View, Image } from "react-native";
 import { getDetails } from "../../common/redux/actions";
 import { useDispatch, useSelector } from "react-redux";
+import styles from "./CardDetailStyles";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 export default function CardDetail({ route }) {
   const dispatch = useDispatch();
@@ -15,26 +17,39 @@ export default function CardDetail({ route }) {
   // const startDate = details.start.toString().slice(4, 15);
   // const startHour = details.start.toString().slice(16, 24);
 
-  // console.log("este es el detail:", details);
+   console.log("este es el detail:", details);
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <View style={styles.view}>
+      
+      
       {details.length ? (
-        <View>
-          <Text style={{ textAlign: "center" }}>{details[0].title}</Text>
-          <Text style={{ textAlign: "center" }}>{details[0].description}</Text>
-          <Text style={{ textAlign: "center" }}>{details[0].fee}</Text>
+        <View style={styles.view2}>
+          <Text style={styles.title}>{details[0].title}</Text>
+          <View style={{marginBottom:'20%'}}>
           <View>
-            <Image style={{ resizeMode: "contain", height: 300, width: "100%" }} source={{ uri: `${details[0].photo}` }} />
+            <Image style={styles.img} source={{ uri: `${details[0].photo}` }} />
           </View>
+          <View style={{marginBottom:'15%'}} >
+          <Text style={{ textAlign: "center",fontSize:20 }}>{details[0].description} </Text>
+          </View>
+
+          
+          <View style={{flexDirection:'row',justifyContent:'space-between'}}>
+          <Text style={{ textAlign: "center" }}>${details[0].fee}</Text>
           <Text style={{ textAlign: "center" }}>{details[0].category}</Text>
+          </View>
+
           <Text style={{ textAlign: "center" }}>{`El evento será el día ${details[0].start.toString().slice(4, 15)} a las ${details[0].start.toString().slice(16, 24)}`}</Text>
+          
+        </View>
         </View>
       ) : (
         <View>
           <Text>Loading...</Text>
         </View>
       )}
-    </View>
+      </View>
+    
   );
 }
