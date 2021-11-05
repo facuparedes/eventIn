662e559ae053bcc3b8ecf7133364f4f7ce15247b
LucianaHer <lucianaher74@gmail.com>
2021-11-05 20:12:33 -0300

    findByDate in process

--- src/common/redux/actions.js
+++ src/common/redux/actions.js
@@ -4,8 +4,9 @@ import { where } from "firebase/firestore"
 export const GET_EVENTS = "GET_EVENTS";
 export const GET_DETAILS = "GET_DETAILS";
 export const GET_EVENTS_BY_TITLE = "GET_EVENTS_BY_TITLE";
-export const GET_EVENTS_CATEGORY = "GET_EVENTS_CATEGORY"
+export const GET_EVENTS_CATEGORY = "GET_EVENTS_CATEGORY";
 export const IS_LOGGED = "IS_LOGGED";
+export const GET_EVENTS_DATE= "GET_EVENTS_DATE";
 
 export const getEvents = () => {
   return async function (dispatch) {
@@ -20,11 +21,29 @@ export const getEvents = () => {
 
 export const getEventsByCategory = (category) => {
   return async function (dispatch) {
-    let result = await event.find(where("category", "==", category));
-    return dispatch({
-      type: GET_EVENTS_CATEGORY,
-      payload: result,
-    });
+    var resultCat=[]
+    resultCat = await event.find(where("category", "==", category));
+    if(resultCat.length>0){
+      console.log("PROP start FIREBASE",resultCat[0].start)
+      return dispatch({
+        type: GET_EVENTS_CATEGORY,
+        payload: resultCat,
+      });
+    } else alert("No hay eventos en esa Categoría")
+  };
+};
+
+export const getEventsByDate = (date) => {
+  console.log("ACTION: ", date)
+  return async function (dispatch) {
+    let resultDate = await event.find(where("start", "==", date));
+    console.log("EVENTO X FECHA: ",resultDate)
+    if(resultDate.length>0){
+      return dispatch({
+        type: GET_EVENTS_DATE,
+        payload: resultDate,
+      });
+    }else alert("no hay eventos en esa fecha")
   };
 };
 
