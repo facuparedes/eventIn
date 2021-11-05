ebdc8a880529b24dd067938ce5fc3863be696560
CrisCorzo97 <CrCorzo04@hotmail.com>
2021-11-05 12:14:48 -0300

    first changes in cardDetail

--- src/screens/Card Detail/CardDetail.jsx
+++ src/screens/Card Detail/CardDetail.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect } from "react";
-import { Text, View, Image } from "react-native";
+import { Text, View, Image, TouchableOpacity } from "react-native";
 import { getDetails } from "../../common/redux/actions";
 import { useDispatch, useSelector } from "react-redux";
 import styles from "./CardDetailStyles";
+import { AntDesign, FontAwesome } from "@expo/vector-icons";
+import moment from "moment";
 
-export default function CardDetail({ route }) {
+export default function CardDetail({ route, navigation }) {
   const dispatch = useDispatch();
   const { id } = route.params;
   useEffect(() => {
@@ -14,38 +16,42 @@ export default function CardDetail({ route }) {
   const details = useSelector((state) => state.detail);
 
   return (
-      <View style={styles.view}>
-      {
-        details.length ? (
-          <View>
-            <View style={styles.viewLine}></View>
-            <Text style={styles.title}>{details[0].title.toUpperCase()}</Text>
+    <View style={styles.view}>
+      {details.length ? (
+        <View style={styles.viewCont}>
+          <View style={styles.header}>
             <Image style={styles.img} source={{ uri: `${details[0].photo}` }} />
-
-            <View style={styles.textConts}>
-              <View style={styles.viewCont}>
-                <Text style={styles.bold}>Sobre este evento </Text><Text>{details[0].description}</Text>
-              </View>
-
-              <View style={styles.viewCont}>
-                <Text style={styles.bold}>Valor </Text><Text>${details[0].fee}</Text>
-              </View>
-              
-              <View style={styles.viewCont}>
-                <Text style={styles.bold}>Categoría </Text><Text>{details[0].category}</Text>
-              </View>
-
-              <View style={styles.viewCont}>
-                <Text style={[styles.bold, {paddingBottom: 12}]}>Fecha </Text><Text>{details[0].start.toString().slice(4, 15)} {details[0].start.toString().slice(16, 24)}</Text>
-              </View>
-              <View style={styles.viewContLine}></View> 
-              <View style={styles.viewLine}></View>
+            <TouchableOpacity style={styles.btnBack} onPress={() => navigation.navigate("TabBar")}>
+              <AntDesign name="leftcircle" size={30} color="rgba(255, 255, 255, 0.6)" />
+            </TouchableOpacity>
+            <View style={styles.btnLikeBackground}>
+              <FontAwesome name="circle" size={45} color="rgba(255, 255, 255, 0.6)" style={{ marginRight: 15 }} />
+              <FontAwesome name="circle" size={45} color="rgba(255, 255, 255, 0.6)" />
             </View>
-
+            <TouchableOpacity style={styles.btnLike}>
+              <AntDesign name="heart" size={24} color="white" />
+            </TouchableOpacity>
           </View>
-        ) : (
-          <Text>Loading...</Text>
+          <Text style={styles.title}>{details[0].title.toUpperCase()}</Text>
+          <View style={styles.textConts}>
+            <Text style={styles.bold}>Sobre este evento </Text>
+            <Text>{details[0].description}</Text>
+
+            <Text style={styles.bold}>Valor </Text>
+            <Text>${details[0].fee}</Text>
+
+            <Text style={styles.bold}>Categoría </Text>
+            <Text>{details[0].category}</Text>
+
+            <Text style={[styles.bold, { paddingBottom: 12 }]}>Fecha </Text>
+            <Text>
+              {details[0].start.toString().slice(4, 15)} {details[0].start.toString().slice(16, 24)}
+            </Text>
+          </View>
+        </View>
+      ) : (
+        <Text>Loading...</Text>
       )}
-      </View>
+    </View>
   );
 }
--- src/screens/Card Detail/CardDetailStyles.js
+++ src/screens/Card Detail/CardDetailStyles.js
@@ -1,54 +1,72 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
+import { colorPallete } from "../Onboarding/styles";
 
-// first: "#2968c0", second: "#298bc4", third: "#29adbf", fourth: "#6ad5ce", fifth: "#d7eae9" };
+const { width, height } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
-    view: {
-        flex: 1,
-        alignContent: "center",
-        justifyContent: "center",
-        backgroundColor: '#fff',
-    },
-    img: {
-        resizeMode: "cover",
-        height: 250,
-        width: "136%",
-        borderRadius: 4,
-        justifyContent: 'center',
-        alignSelf: 'center',
-    },
-    title: {
-        fontSize: 30,
-        marginBottom: 10,
-        borderTopWidth: 1,
-        marginLeft: 0,
-        paddingTop: 16,
-        borderTopColor: '#6ad5ce'
-    },
-    viewLine: {
-        borderBottomWidth: 2,
-    },
-    viewTitle: {
-        alignSelf: 'flex-start',
-        backgroundColor: '#d7eae9'
-    },
-    bold: {
-        fontWeight: 'bold'
-    },
-    viewCont: {
-        flexDirection: 'row',
-        fontSize: 20,
-        marginTop: 4
-    },
-    textConts: {
-        marginLeft: 6,
-        marginTop: 10
-    },
-    viewContLine: {
-        borderBottomWidth: 1,
-        borderBottomColor: '#6ad5ce',
-        marginLeft: 0
-    }
+  view: {
+    flex: 1,
+    alignContent: "center",
+    justifyContent: "flex-start",
+    backgroundColor: "grey",
+  },
+  viewCont: {
+    fontSize: 20,
+    marginTop: 4,
+  },
+  header: {
+    width: "100%",
+    height: height * 0.385,
+    paddingTop: 20,
+  },
+  btnBack: {
+    position: "absolute",
+    paddingTop: 30,
+    paddingLeft: 10,
+  },
+  btnLikeBackground: {
+    flex: 1,
+    flexDirection: "row",
+    position: "absolute",
+    alignSelf: "flex-end",
+    paddingTop: height * 0.335,
+    paddingRight: width * 0.05,
+  },
+  btnLike: {
+    position: "absolute",
+  },
+  img: {
+    position: "relative",
+    resizeMode: "cover",
+    width: width / 1,
+    height: height / 2.7,
+    justifyContent: "center",
+    alignSelf: "center",
+  },
+  title: {
+    fontSize: 30,
+    marginBottom: 10,
+    borderTopWidth: 1,
+    marginLeft: 0,
+    paddingTop: 16,
+    borderTopColor: "#6ad5ce",
+  },
+  viewTitle: {
+    alignSelf: "flex-start",
+    backgroundColor: "#d7eae9",
+  },
+  bold: {
+    fontWeight: "bold",
+  },
+  textConts: {
+    marginLeft: 6,
+    marginTop: 10,
+  },
+  viewContLine: {
+    borderBottomWidth: 1,
+    borderBottomColor: "#6ad5ce",
+    marginLeft: 0,
+  },
 });
 
 export default styles;
