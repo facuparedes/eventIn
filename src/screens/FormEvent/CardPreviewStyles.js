a8eaf779e8ff138bd50729c517691c8bdebb0a3a
santicardona04 <santiago.cardona1@gmail.com>
2021-11-04 23:42:30 -0300

    piprincipio de los estilos del preview

---
+++ src/screens/FormEvent/CardPreviewStyles.js
@@ -0,0 +1,95 @@
+import { StyleSheet ,Dimensions } from 'react-native';
+// .color1 {color: #2968c0;}
+// .color2 {color: #298bc4;}
+// .color3 {color: #29adbf;}
+// .color4 {color: #6ad5ce;}
+// .color5 {color: #d7eae9;}
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+const estilos = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        paddingHorizontal: 8
+    },
+    logoImage: {
+        resizeMode: 'contain',
+        height: 60,
+        width: 80,
+        marginLeft: 100
+    },
+    textAndImg: {
+        flexDirection: 'row',
+        height: 60,
+        marginBottom:70,
+        marginTop:10
+    },
+    card_body_image: {
+        position: "relative",
+        // width: "100%",
+        // height: windowHeight / 2.5,
+        width: windowWidth / 1.17,
+        height: windowHeight / 3.4,
+        borderRadius: 15,
+      },
+      titleText: {
+        textAlign: 'left',
+        marginTop: 10
+    },
+    card_body: {
+        width: "100%",
+      },
+    cardContainer:{
+        flex:1,
+        backgroundColor: "#ffffff",
+        borderRadius: 20,
+        elevation: 10,
+        // alignItems: "center",
+        justifyContent: "space-around",
+        paddingHorizontal: 15,
+        paddingVertical:20,
+        marginTop:10,
+        marginBottom:10,
+        // marginVertical: 45,
+    },
+    cardItems:{
+        alignItems:'center',
+        justifyContent:'space-evenly',
+        padding:7
+    },
+    card_header:{
+    // flex: 1,
+    paddingHorizontal: 6,
+    // paddingVertical: 10,
+    width: "100%",
+    },
+    card_header_title: {
+        fontFamily: "Gotham-Medium",
+        fontSize: 15,
+        // color: "#000",
+        color: '#298bc4',
+    },
+    card_header_description: {
+        color: "#5c6066",
+        fontFamily: "Gotham-Book",
+        fontSize: 13,
+      },
+      card_body_date: {
+        position: "absolute",
+        right: 15,
+        fontSize: 14,
+        color: "white",
+        marginHorizontal: 5,
+        marginVertical: 8,
+        borderRadius: 100,
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        backgroundColor: "rgba(255, 255, 255, 0.4)",
+      },
+      card_body_date_active: {
+        backgroundColor: "#6ad5ce",
+        color: "white",
+        
+      },
+})
+export default estilos
--- src/screens/FormEvent/FormCardPreview.jsx
+++ src/screens/FormEvent/FormCardPreview.jsx
@@ -5,9 +5,9 @@ import { TouchableOpacity } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useSelector } from "react-redux";
 import formStyles from "./FormStyles";
-import {styles} from "../../common/components/Card/styles";
 import Event from '../../../api/firebase/models/event'
 import moment from "moment";
+import estilos from "./CardPreviewStyles";
 
 const FormCardPreview = ({ navigation }) => {
   const eventInfo = useSelector((state) => state.eventForm);
@@ -20,26 +20,29 @@ const FormCardPreview = ({ navigation }) => {
     navigation.replace("Home")
   }
   return (
-    <SafeAreaView style={formStyles.container}>
+    <SafeAreaView style={estilos.container}>
       <LinearProgress color="lightgreen" variant="determinate" value={0.9} />
-      <View style={(formStyles.textAndImg, { marginRight: 10 })}>
-        <Text h4 style={formStyles.titleText}>
+      <View style={(estilos.textAndImg)}>
+        <Text h4 style={estilos.titleText}>
           Preview del Evento
         </Text>
-        <Image source={require("../../assets/Logo.png")} style={formStyles.logoImage} />
+        <Image source={require("../../assets/Logo.png")} style={estilos.logoImage} />
       </View>
+      
+      <View style={estilos.cardContainer}>
       {/*CARD PREVIEW*/}
-      <View style={styles.card}>
-        <View style={{marginLeft: '45%'}}>
-          <Text style={styles.card_header_title}>{eventInfo.title}</Text>
-          <Text numberOfLines={3} style={styles.card_header_title_description}>
-            {eventInfo.description}
-          </Text>
+      
+        <View style={estilos.card_header}>
+        <View style={estilos.cardItems}>
+          <Text style={estilos.card_header_title}>{eventInfo.title} TITULO</Text>
+          <Text numberOfLines={3} style={estilos.card_header_description}> {eventInfo.description} DESCRIPCION </Text>
         </View>
-        <View style={styles.card_body}>
-          <Image source={{ uri: eventInfo.photo }} style={styles.card_body_image} resizeMode={"cover"} />
-          <Text style={[styles.card_body_date, isToday ? styles.card_body_date_active : ""]}>{moment(eventInfo.start.date).toNow()}</Text>
+        <View style={estilos.card_body}>
+          <Image source={{ uri: eventInfo.photo }} style={estilos.card_body_image} resizeMode={"cover"} />
+          <Text style={[estilos.card_body_date, isToday ? estilos.card_body_date_active : ""]}>{moment(eventInfo.start.date).toNow()}</Text>
         </View>
+        </View>
+      
       </View>
 
       <View style={formStyles.btnsContainer}>
