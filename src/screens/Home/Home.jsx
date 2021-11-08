bcdf0e7c36fda73ddfc364beb4532359634d11f3
pruscius <andi_frank19@hotmail.com>
2021-11-08 04:45:34 -0300

    added pull refresh and ignoreLog for flatList in ScrollView (must solve)

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { changeIsLogged, getEvents, getEventsByCategory } from "../../common/redux/actions";
 import { onAuthStateChanged } from "firebase/auth";
-import { View } from "react-native";
+import { View, SafeAreaView, RefreshControl, ScrollView } from "react-native";
 import { styles } from "./styles.js";
 import { Picker } from "@react-native-picker/picker";
 import auth from "../../../api/firebase/services/AuthService";
@@ -12,6 +12,18 @@ import { categoryArray } from "../../common/categories";
 
 export default function Home() {
   const dispatch = useDispatch();
+
+  const [refreshing, setRefreshing] = useState(false);
+
+  const wait = (timeout) => {
+    return new Promise(resolve => setTimeout(resolve, timeout));
+  }
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    dispatch(getEvents());
+    wait(2000).then(() => setRefreshing(false));
+  }, []);
 
   const [categ, setCateg] = useState("Categoría");
   var categArray2 = [...categoryArray];
@@ -41,28 +53,38 @@ export default function Home() {
   }
 
   return (
-    <View style={styles.container}>
-      <View style={styles.filtersContainer}>
-        <View style={styles.viewPicker}>
-          <Picker
-            selectedValue={categ}
-            onValueChange={(value, index) => handleFilterCategory(value)}
-            mode="dropdown" // Android only
-            style={styles.picker}
-          >
-            {categArray2.map((item, i) => {
-              return <Picker.Item key={i} value={item} label={item} />;
-            })}
-          </Picker>
+    <SafeAreaView style={styles.container}>
+      <ScrollView
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={onRefresh}
+          />
+        }
+      >
+      
+        <View style={styles.filtersContainer}>
+          <View style={styles.viewPicker}>
+            <Picker
+              selectedValue={categ}
+              onValueChange={(value, index) => handleFilterCategory(value)}
+              mode="dropdown" // Android only
+              style={styles.picker}
+            >
+              {categArray2.map((item, i) => {
+                return <Picker.Item key={i} value={item} label={item} />;
+              })}
+            </Picker>
+          </View>
+          <View style={styles.datePicker}>
+            <DatePicker />
+          </View>
         </View>
-        <View style={styles.datePicker}>
-          <DatePicker />
-        </View>
-      </View>
 
-      <View style={{ flex: 1, alignItems: "center", width: "100%" }}>
-        <CardsFlat />
-      </View>
-    </View>
+        <View style={{ flex: 1, alignItems: "center", width: "100%" }}>
+          <CardsFlat />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
