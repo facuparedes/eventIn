628802240365934185b84d05e6fb418a6ff768dd
pruscius <andi_frank19@hotmail.com>
2021-11-09 20:14:57 -0300

    fixed bugs

--- src/screens/Edit Profile/EditProfile.jsx
+++ src/screens/Edit Profile/EditProfile.jsx
@@ -9,6 +9,23 @@ import { useSelector } from 'react-redux';
 import auth from "../../../api/firebase/services/AuthService";
 import { updatePassword, reauthenticateWithCredential, EmailAuthProvider } from "@firebase/auth";
 
+function validate (passwords) {
+    let errors = {};
+    if (!passwords.oldPassword) {
+        errors.oldPassword = 'Por favor, ingresa tu contraseña actual.'
+    }
+    if (passwords.oldPassword.length < 8) {
+        errors.oldPasswordLength = 'Error en tu contraseña actual.'
+    }
+    if(!passwords.newPassword) {
+        errors.newPassword = 'Debes ingresar una nueva contraseña.'
+    }
+    if(passwords.newPassword.length < 8) {
+        errors.newPasswordLength = 'Tu contraseña nueva debe contener al menos 8 carácteres.'
+    }
+    return errors;
+}
+
 export default function EditProfile({navigation}) {
     const [newPassword, setNewPassword] = useState('');
     const [oldPassword, setOldPassword] = useState('');
@@ -30,27 +47,35 @@ export default function EditProfile({navigation}) {
         setSelectedImage({localUri: pickerResult.uri})
         }
 
-        const getCredential = async () => {
-            const authCredential = EmailAuthProvider.credential(auth.currentUser.email, oldPassword);
-            try {
-                const userCredentail = await reauthenticateWithCredential(auth.currentUser, authCredential)  
-                return userCredentail;              
-            }catch(e) {
-                console.log('ERRORRRRR', e)
-            }
-        }
 
-        const changePassword = () => {
-            getCredential()
-                .then(user => {
-                    updatePassword(auth.currentUser, newPassword)
-                })
-                .then(res => {
-                    console.log('SUCCESS');
-                    return Alert.alert('Contraseña cambiada.');
-                })
-                .catch(e=>console.log(e));
+    const changePassword = () => {
+
+        const validation = validate({oldPassword: oldPassword, newPassword: newPassword});
+
+        if (Object.keys(validation).length === 0) {
+            
+            const authCredential = EmailAuthProvider.credential(auth.currentUser.email, oldPassword);
+
+            reauthenticateWithCredential(auth.currentUser, authCredential)
+            .then(res => {
+                updatePassword(auth.currentUser, newPassword)
+            })
+            .then(res => {
+                return Alert.alert('Contraseña actualizada.');
+            })
+            .catch(e => {
+                const errorMessage = e.message;
+                if (errorMessage === 'Firebase: Error (auth/internal-error).') {
+                    return Alert.alert('Por favor, ingresa tu contraseña actual.');
+                }
+                if (errorMessage === 'Firebase: Error (auth/wrong-password).') {
+                    return Alert.alert('Contraseña actual incorrecta.');
+                }
+            })
+        } else {
+            return Alert.alert(`${Object.values(validation)[0]}`);
         }
+    }
     
     
     return(
@@ -68,13 +93,19 @@ export default function EditProfile({navigation}) {
             <Input label="Mail" placeholder="Escribe tu mail">{user.email}</Input>
             </View>
             <View style={{alignSelf: 'center'}}>
-                <TextInput placeholder='vieja contraseña'onChangeText={(text) => setOldPassword(text)}/>
+                <TextInput 
+                    placeholder='Contraseña actual' 
+                    onChangeText={(text) => setOldPassword(text)}
+                    />
             </View>
             <View style={{alignSelf: 'center'}}>
-                <TextInput placeholder='nueva contraseña'onChangeText={(text) => setNewPassword(text)}/>
+                <TextInput 
+                    placeholder='Contraseña nueva' 
+                    onChangeText={(text) => setNewPassword(text)}
+                    />
             </View>
             <TouchableOpacity style={{alignSelf: 'center', margin: 10, borderWidth: 2}}onPress={changePassword}>
-                <Text>Actualizar</Text>
+                <Text>Cambiar contraseña</Text>
             </TouchableOpacity>
         </SafeAreaView>
     )
