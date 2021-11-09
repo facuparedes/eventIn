58c8949412586db8c076dbae677d27f5fbdb67bd
pruscius <andi_frank19@hotmail.com>
2021-11-09 03:03:25 -0300

    added update password firebase logic

--- src/screens/Edit Profile/EditProfile.jsx
+++ src/screens/Edit Profile/EditProfile.jsx
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
-import { View , Text, Image, SafeAreaView} from "react-native";
+import { View , Text, Image, SafeAreaView, Alert } from "react-native";
 import { Input } from "react-native-elements";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import { TextInput, TouchableOpacity } from "react-native-gesture-handler";
 import styles from "./styles"
 import { Feather , AntDesign } from '@expo/vector-icons'; 
 import * as ImagePicker from "expo-image-picker"
 import { useSelector } from 'react-redux';
+import auth from "../../../api/firebase/services/AuthService";
+import { updatePassword, reauthenticateWithCredential, EmailAuthProvider } from "@firebase/auth";
 
 export default function EditProfile({navigation}) {
-const user = useSelector(state=>state.isLogged);
+    const [newPassword, setNewPassword] = useState('');
+    const [oldPassword, setOldPassword] = useState('');
+
+    const user = useSelector(state=>state.isLogged);
 
     const [selectedImage, setSelectedImage] = useState(null)
     let openImagePickerAsync = async()=>{
@@ -23,6 +28,28 @@ const user = useSelector(state=>state.isLogged);
             return
         }
         setSelectedImage({localUri: pickerResult.uri})
+        }
+
+        const getCredential = async () => {
+            const authCredential = EmailAuthProvider.credential(auth.currentUser.email, oldPassword);
+            try {
+                const userCredentail = await reauthenticateWithCredential(auth.currentUser, authCredential)  
+                return userCredentail;              
+            }catch(e) {
+                console.log('ERRORRRRR', e)
+            }
+        }
+
+        const changePassword = () => {
+            getCredential()
+                .then(user => {
+                    updatePassword(auth.currentUser, newPassword)
+                })
+                .then(res => {
+                    console.log('SUCCESS');
+                    return Alert.alert('Contraseña cambiada.');
+                })
+                .catch(e=>console.log(e));
         }
     
     
@@ -40,6 +67,15 @@ const user = useSelector(state=>state.isLogged);
             <Input label="Nombre" placeholder="Escribe tu nombre">{user.username}</Input>
             <Input label="Mail" placeholder="Escribe tu mail">{user.email}</Input>
             </View>
+            <View style={{alignSelf: 'center'}}>
+                <TextInput placeholder='vieja contraseña'onChangeText={(text) => setOldPassword(text)}/>
+            </View>
+            <View style={{alignSelf: 'center'}}>
+                <TextInput placeholder='nueva contraseña'onChangeText={(text) => setNewPassword(text)}/>
+            </View>
+            <TouchableOpacity style={{alignSelf: 'center', margin: 10, borderWidth: 2}}onPress={changePassword}>
+                <Text>Actualizar</Text>
+            </TouchableOpacity>
         </SafeAreaView>
     )
 }
