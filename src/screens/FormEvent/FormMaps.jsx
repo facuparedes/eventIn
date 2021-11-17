35b67d20e5e1af268997b538246643ec73fb6a09
santicardona04 <santiago.cardona1@gmail.com>
2021-11-17 19:26:30 -0300

    mapa con search bar funcionando

--- src/screens/FormEvent/FormMaps.jsx
+++ src/screens/FormEvent/FormMaps.jsx
@@ -21,12 +21,12 @@ const FormMaps = ({ navigation }) => {
     latitude: -34.667270557115565,
     longitude: -58.368570803061345,
   });
-  console.log("esta es la ubicacion actual", pin);
+  // console.log("esta es la ubicacion actual", pin);
   const [region, setRegion] = useState({
     latitude: -34.667270557115565,
     longitude: -58.368570803061345,
-    latitudeDelta: 0.0000000000000004,
-    longitudeDelta: 0.003421,
+    latitudeDelta: 0.000000000004,
+    longitudeDelta: 0.002421,
   });
 
   const [errorMsg, setErrorMsg] = useState(null);
@@ -90,40 +90,35 @@ const FormMaps = ({ navigation }) => {
         <TouchableOpacity title="elegir ubicacion" onPress={handleShowMap} style={styles.btn2}>
           <Text style={styles.textMaps}>Mostrar mapa</Text>
         </TouchableOpacity>
-        {/* <GooglePlacesAutocomplete
-          placeholder="Search"
-          onPress={(data, details = null) => {
-            // 'details' is provided when fetchDetails = true
-            console.log(data, details);
-            setRegion({
-              latitude: details.geometry.location.lat,
-              longitude: details.geometry.location.lng,
-              latitudeDelta: 0.0000000000000004,
-              longitudeDelta: 0.003421,
-            });
-          }}
-          fetchDetails={true}
-          GooglePlacesSearchQuery={{ rankby: "distance" }}
-          query={{
-            key: "AIzaSyDHw9p6ttvBYzEy148PIWBnhdNd59jqxjo",
-            language: "en",
-            components: "country:argentina",
-            types: "establishment",
-            radius: 30000,
-            location: `${region.latitude},${region.longitude}`,
-          }}
-          styles={{
-            container: { 
-              flex: 0, 
-              width: "100%", 
-              zIndex: 1, 
-              borderWidth: 1, 
-              borderRadius: 6, 
-              height: 50 ,
-            },
-            listView: { backgroundColor: "white" },
-          }}
-        /> */}
+        <GooglePlacesAutocomplete
+				placeholder="Search"
+				fetchDetails={true}
+				GooglePlacesSearchQuery={{
+					rankby: "distance"
+				}}
+				onPress={(data, details = null) => {
+					// 'details' is provided when fetchDetails = true
+					// console.log(data, details)
+					setPin({
+						latitude: details.geometry.location.lat,
+						longitude: details.geometry.location.lng,
+						latitudeDelta: 0.0922,
+						longitudeDelta: 0.0421
+					})
+				}}
+				query={{
+					key: "AIzaSyDEvbPWfuQvaChx1QrpAPgj_DiXB6R-6Ys",
+					language: "es",
+					components: "country:arg",
+					types: "establishment",
+					radius: 30000,
+					location: `${region.latitude}, ${region.longitude}`
+				}}
+				styles={{
+					container: { flex: 0, position: "relative", width: "100%", zIndex: 1 },
+					listView: { backgroundColor: "white" }
+				}}
+			/>
         <View style={styles.containerMap}>
           {showMap && (
             <MapView
@@ -131,34 +126,30 @@ const FormMaps = ({ navigation }) => {
               initialRegion={{
                 latitude: pin.latitude,
                 longitude: pin.longitude,
-                latitudeDelta: 0.0000000000000004,
-                longitudeDelta: 0.003421,
+                latitudeDelta: 0.000000000004,
+                longitudeDelta: 0.002421,
               }}
               provider="google"
             >
-              {/* <Marker coordinate={{latitude: region.latitude, longitude: region.longitude}}> 
-          <Callout>
-                <Text> estadio </Text>
-              </Callout>
-              </Marker> */}
-
-              <Marker
-                coordinate={pin}
-                draggable={true}
-                // onDragStart={(e) => {
-                //     console.log("Drag start", e.nativeEvent.coordinate);
-                //   }}
-                onDragEnd={(e) => {
-                  setPin({
-                    latitude: e.nativeEvent.coordinate.latitude,
-                    longitude: e.nativeEvent.coordinate.longitude,
-                  });
-                }}
-              >
-                <Callout>
-                  <Text>el mejor estadio del mundo</Text>
-                </Callout>
-              </Marker>
+              
+              	<Marker
+					coordinate={pin}
+					pinColor="black"
+					draggable={true}
+					onDragStart={(e) => {
+						console.log("Drag start", e.nativeEvent.coordinates)
+					}}
+					onDragEnd={(e) => {
+						setPin({
+							latitude: e.nativeEvent.coordinate.latitude,
+							longitude: e.nativeEvent.coordinate.longitude
+						})
+					}}
+				>
+					<Callout>
+						<Text>I'm here</Text>
+					</Callout>
+				</Marker>
             </MapView>
           )}
         </View>
