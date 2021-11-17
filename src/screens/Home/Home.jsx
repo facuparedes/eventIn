409f431189aa49f8423beac0bd361a012e6b207e
Christian-000 <christianpiccinini4@gmail.com>
2021-11-17 17:57:37 -0300

    thinkAboutUsrExp

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
-import { changeIsLogged, getEvents, getEventsByCategory } from "../../common/redux/actions";
+import React, { useEffect, useState, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { changeIsLogged, getEvents, getEventsByCategory, getLikedEvents } from "../../common/redux/actions";
 import { onAuthStateChanged } from "firebase/auth";
-import { View, SafeAreaView, RefreshControl, ScrollView, TouchableOpacity, Text } from "react-native";
+import { View, SafeAreaView, RefreshControl, ScrollView, TouchableOpacity, Text, Button, Alert, Platform } from "react-native";
 import { styles } from "./styles.js";
 import { Picker } from "@react-native-picker/picker";
 import auth from "../../../api/firebase/services/AuthService";
@@ -10,9 +10,43 @@ import CardsFlat from "../../common/components/CardsFlat/CardsFlat";
 import DatePicker from "../../common/components/DatePicker/DatePicker";
 import { categoryArray } from "../../common/categories";
 import event from "../../../api/firebase/models/event";
+import * as Notifications from "expo-notifications";
+import Constants from "expo-constants";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function Home() {
   const dispatch = useDispatch();
+
+  const [expoPushToken, setExpoPushToken] = useState("");
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+  useEffect(() => {
+    dispatch(getLikedEvents(auth.currentUser.uid));
+    registerForPushNotificationsAsync().then((token) => setExpoPushToken(token));
+
+    notificationListener.current = Notifications.addNotificationReceivedListener((notification) => {
+      setNotification(notification);
+    });
+
+    responseListener.current = Notifications.addNotificationResponseReceivedListener((response) => {
+      console.log(response);
+    });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener.current);
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
+  const allLikedEvents = useSelector((state) => state.likedEvents);
 
   const [refreshing, setRefreshing] = useState(false);
 
@@ -53,6 +87,35 @@ export default function Home() {
       dispatch(getEventsByCategory(value));
     }
   }
+  setTimeout(() => {
+    const todayEvents = allLikedEvents.filter((ev) => {
+      let diff = Math.abs(new Date().getTime() - ev.start.getTime());
+      let days = diff / (1000 * 60 * 60 * 24);
+      days = Math.floor(days);
+      if (days === 0) return ev;
+    });
+    if (todayEvents.length > 0) {
+      sendMessage(expoPushToken);
+    }
+  }, 3600000);
+
+  const sendMessage = (token) => {
+    fetch("https://exp.host/--/api/v2/push/send", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Accept-encoding": "gzip, deflate",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        to: token,
+        title: "¡Corre, no te lo pierdas!",
+        body: "Uno de tus elementos likeados se realiza hoy!",
+        data: { data: "goes here" },
+        _displayInForeground: true,
+      }),
+    });
+  };
 
   return (
     <SafeAreaView style={styles.container}>
@@ -82,7 +145,49 @@ export default function Home() {
   );
 }
 
-{/* <TouchableOpacity onPress={() => {
+async function schedulePushNotification() {
+  await Notifications.scheduleNotificationAsync({
+    content: {
+      title: "You've got mail! 📬",
+      body: "Here is the notification body",
+      data: { data: "goes here" },
+    },
+    trigger: { seconds: 2 },
+  });
+}
+
+async function registerForPushNotificationsAsync() {
+  let token;
+  if (Constants.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== "granted") {
+      alert("Failed to get push token for push notification!");
+      return;
+    }
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+  } else {
+    alert("Tenes que usar un celular verdadero para recibir push-notifications salamin");
+  }
+
+  if (Platform.OS === "android") {
+    Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+
+  return token;
+}
+{
+  /* <TouchableOpacity onPress={() => {
   event.create({
     attachments: [
       "file:///data/user/0/host.exp.exponent/cache/ExperienceData/%2540andyfrank%252Fproyecto-grupal/ImagePicker/a9ad3ae9-3aff-48b5-8697-27696cd77a29.jpg",
@@ -116,4 +221,5 @@ export default function Home() {
 
 }}>
   <Text>EVENT</Text>
-</TouchableOpacity> */}
+</TouchableOpacity> */
+}
