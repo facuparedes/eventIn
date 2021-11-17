7c416362be1685aca45171d7b3292d400c8640be
Christian-000 <christianpiccinini4@gmail.com>
2021-11-16 23:28:43 -0300

    firebaseConfigHidden

--- api/firebase/config.js
+++ api/firebase/config.js
@@ -3,17 +3,18 @@ import { initializeApp } from "firebase/app";
 import { setLogLevel } from "@firebase/firestore";
 import { initializeFirestore } from "@firebase/firestore";
 import { LogBox } from "react-native";
+import * as _var from '../vars';
 LogBox.ignoreLogs(['Setting a timer', 'VirtualizedLists should never be nested']);
 // setLogLevel('debug')
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD6jUHn_5STRnE4jsNqjSQhXWdApjmFQUw",
-  authDomain: "unnamed-522b2.firebaseapp.com",
-  projectId: "unnamed-522b2",
-  storageBucket: "unnamed-522b2.appspot.com",
-  messagingSenderId: "99982117887",
-  appId: "1:99982117887:web:81ff0ed4f5d64457ddf5ca",
-  measurementId: "G-TGZYFR0G0T",
+  apiKey: _var.API_KEY,
+  authDomain: _var.AUTH_DOMAIN,
+  projectId: _var.PROJECT_ID,
+  storageBucket: _var.STORAGE_BUCKET,
+  messagingSenderId: _var.MESSAGING_SENDER_ID,
+  appId: _var.APP_ID,
+  measurementId: _var.MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
